6583ca73d9881b1d0c9b9863ba299bdec9cd5586
Unknown <gregoire.dervaux@2019.icam.fr>
2018-09-04 23:09:11 +0200

    beta index
    dessin grossier de l'accueil

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-<?php header('Location: app/raid/controllers/index_to_accueil.php'); ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Raid Icam</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="retrouvez le site web de l'association Raid Icam"/>
+	<link rel="shortcut icon" href="library/img/favicon.ico"> 
+	<link rel="stylesheet" href="app/style/index.css">
+
+</head>
+<body>
+	<div class="background">
+		<img src="/library/img/fond_index.png">
+	</div>
+	<div class="casque">
+		<img src="/library/img/casque.png">
+	</div>
+	<div class="cache"></div>
+	<div class="corps">
+		<section class="trail column">
+			<div class="container">
+				<img src="/library/img/trail.png">
+			</div>
+		</section>
+		<section class="column path">
+			
+		</section>
+		<section class="raid column">
+			<div class="container">
+				<img src="/library/img/raid.png">
+			</div>
+		</section>
+	</div>
+
+</body>
+</html>
