e9c0cbfc1fe17c0cdff5ed2ecd963f24922354da
Unknown <gregoire.dervaux@2019.icam.fr>
2018-09-14 10:53:14 -0400

    page de maintenance et lien depuis raid

--- app/controllers/raid/index_to_accueil.php
+++ app/controllers/raid/index_to_accueil.php
@@ -1,23 +1,32 @@
 <?php
 session_start();
+require('../../../config.php');
 require('../../../library/db/db.php');
 
-$db = new db;
-$ls_sponsor=$db->get_sponsor();
-
-$_SESSION['sponsor']=[];
-#on nettoye les donnée et on les stoques dans la session.
-foreach ($ls_sponsor as $num_part => $ls_valeur)
+if($settings['maintenance']['raid']==1)
 {
-	$_SESSION['sponsor'][$num_part]=[];
-	foreach($ls_valeur as $key => $valeur)
-		if(!is_integer($key))
-		{
-			$_SESSION['sponsor'][$num_part][$key] = $valeur;
-		}	
+	header('Location: ../../views/maintenance.php');
 }
+else
+{
 
-var_dump($_SESSION['sponsor']);
+	$db = new db;
+	$ls_sponsor=$db->get_sponsor();
 
-header('Location: ../../views/raid/accueil.php')
+	$_SESSION['sponsor']=[];
+	#on nettoye les donnée et on les stoques dans la session.
+	foreach ($ls_sponsor as $num_part => $ls_valeur)
+	{
+		$_SESSION['sponsor'][$num_part]=[];
+		foreach($ls_valeur as $key => $valeur)
+			if(!is_integer($key))
+			{
+				$_SESSION['sponsor'][$num_part][$key] = $valeur;
+			}
+	}
+
+	var_dump($_SESSION['sponsor']);
+
+	header('Location: ../../views/raid/accueil.php');
+}
 ?>
---
+++ app/views/maintenance.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Raid Icam</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="retrouvez le site web de l'association Raid Icam"/>
+	<link rel="shortcut icon" href="library/img/favicon.ico"> 
+	<style>
+
+		html{
+			background-image: url('/library/img/fond_index.png');
+			background-size: cover;
+			background-repeat: no-repeat;
+		}
+
+		body{
+			display:flex;
+			justify-content: center;
+			align-items: center;
+			flex-direction: column;
+			height: 100vh;
+			background-color: rgba(255, 165, 0, 0.5);
+			filter: alpha(opacity=30);
+			color:white;
+		}
+		h1{
+			font-family: Capture it;
+			font-size: 3em;
+		}
+    </style>
+</head>
+<body>
+	<h1>Cette section du site n'est pas disponible</h1>
+	<p>si vous pensez qu'il puisse s'agir d'une erreur, alors contactez le raid via <a href="mailto:contact.raidicam@gmail.com">contact.raidicam@gmail.com</a></p>
+</body>
+</html>
