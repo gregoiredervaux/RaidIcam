c51bb4aad2ea8e1aa02e878dbb016477b713b28c
Unknown <gregoire.dervaux@2019.icam.fr>
2018-09-19 10:26:55 -0400

    page inscription trail 1.0

---
+++ app/views/trail/inscription.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inscription</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="retrouvez le site web de l'association Raid Icam"/>
+	<link rel="shortcut icon" href="../../../library/img/favicon.ico"> 
+	<link rel="stylesheet" href="../../style/default.css">
+	<link rel="stylesheet" href="../../style/inscription.css">
+</head>
+<body>
+<?php include('header.php'); ?>
+<section id="titre_principale">
+    <h1>Participez à la première édition de la tiote foulée !</h1>
+    <img class="rounded" id="logo_trail" src="/library/img/real_trail.png">
+</section>
+<section id="descr">
+	<h2>Les inscriptions sont ouvertes !</h2>
+	<p> les inscriptions se font via lydia, notre partenaire de paiement en ligne !</p>
+	<a class="btn" href="">
+		<p>inscrivez vous par ici !</p>
+    	<img src="/library/img/icons/crayon.png" class="icon small">
+	</a>
+</section>
+<?php include('../footer.php');?>
+</body>
